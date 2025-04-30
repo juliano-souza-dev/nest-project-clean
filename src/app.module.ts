5c355765fc893f52d7ad2f992c2d603543313619
Juliano Souza <souzza.s1990@gmail.com>
2025-04-30 07:28:59 -0300

    fix: resolve ConfigModule import error in AuthModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,12 @@ import { ConfigModule } from '@nestjs/config'
 import { AuthModule } from './auth/auth.module'
 
 @Module({
-  imports: [ConfigModule.forRoot({}), AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+  ],
 
   controllers: [CreateAccountController],
   providers: [PrismaService],
